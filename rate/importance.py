077e5f8dadfbec1405035af8ca335fa0efcb01ec
Jonathan Ish-Horowicz <jonathan_ish@hotmail.co.uk>
2021-07-02 09:49:00 +0100

    More space-efficient saving

--- rate/importance.py
+++ rate/importance.py
@@ -240,6 +240,13 @@ def make_rate_result(tracker: rateIterationTracker):
 			pd.DataFrame(x, columns=["S0_pd", "S0_rank", "S0_dim", "S1_pd", "S1_rank", "S1_dim"])
 			for x in tracker.diagnostic_result
 		]
+
+	# if this is one chunk of many we only save the ESA posterior
+	# in the first chunk
+	if tracker.chunk_spec is not None:
+		if chunk_spec[0]>0:
+			tracker.Mb = None
+			tracker.Vb = None
 		  
 	res = rateResult(
 		Mb=tracker.Mb,
